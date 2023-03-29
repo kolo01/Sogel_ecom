6908b113e9db342a77aee9c3288a2732b67988bc
Kone Kolotioloman Dieudonne <Kolotioloman.kone@rschain.net>
2023-03-29 10:35:29 +0000

    first commit

---
+++ pages/Cart.js
@@ -0,0 +1,106 @@
+import FirstNav from "@/components/firstNav";
+import Navbar from "@/components/navbar";
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import { Box, Button, Center, Flex, Image, Input, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { BiStar } from "react-icons/bi";
+
+export default function Cart() {
+  const data = [10, 20, 30];
+  const [data2,setData2] = useState()
+  useEffect(()=>{
+    setData2(0)
+    console.log(data2)
+  })
+  const decrement =()=>{
+    if (data2<0) {
+      const dim= data2-1
+      setData2(dim)
+      console.log(data2)
+    }
+   
+  }
+  const increment =()=>{
+    if (data2<=0) {
+      const sup= data2+1
+      setData2(sup)
+      console.log(data2)
+    }
+   
+  }
+  return (
+    <>
+      <FirstNav />
+      <Navbar />
+      <Flex pb={20}>
+        <Text fontSize={20}> Catégories</Text>
+        <ChevronRightIcon h={8} fontSize={25} />
+        <Text fontSize={20} color={"blue"}>
+          Panier
+        </Text>
+      </Flex>
+      {data.map((data, index) => (
+        <Center>
+          <Flex
+            bgColor={"#F7C29E"}
+            width={"621px"}
+            height={"205px"}
+            border={"1px solid #e6e6e6"}
+            boxShadow={"0px 2px 10px"}
+            boxSizing={"border-box"}
+            borderRadius={"9px"}
+            // pb={10}
+            mb={20}
+          >
+            <Box>
+              <Image
+                src="./images/robe.png"
+                width={"117px"}
+                height={"139px"}
+                ml={15}
+                mt={10}
+              />
+            </Box>
+            <Box>
+              <Text pb={5} pt={5} fontWeight={"bold"}>
+                Zarha
+              </Text>
+              <Flex>
+                <Image src="./images/Star.svg" />
+                <Text as={"sup"} fontSize={12}>
+                  (59)
+                </Text>
+              </Flex>
+              <Text pt={5}>Robe Blazer</Text>
+              <Box
+              
+                borderColor={"#E37611"}
+                borderStyle={"solid"}
+                borderWidth={"0,5px"}
+                as={Flex}
+                
+                borderRadius={"4px"}
+                justifyContent={'space-between'}
+              >
+                <Flex>
+                <Button onclick={()=>decrement()} >-</Button>
+                <Input type={'number'} color={'#E37611'} w={'70px'}  value={data2}borderColor={'#F7C29E'} />
+                <Button  onclick={()=>increment()}>+</Button>
+                </Flex>
+                <Text color={'#E37611'}>3500frs</Text>
+
+              </Box>
+            </Box>
+          </Flex>
+        </Center>
+      ))}
+      <Flex
+      mt={-10}
+      ml={'65%'}
+      mb={'70px'}>
+        
+        <Button bgColor={'#816acd'} borderRadius={50} width={100}>Valider</Button>
+      </Flex>
+    </>
+  );
+}
--- pages/test2.js
+++ pages/test2.js
@@ -1,154 +1,72 @@
-import { ChevronRightIcon, StarIcon } from "@chakra-ui/icons";
-import { Button, Flex, Image, Text } from "@chakra-ui/react";
-import { useRouter } from "next/router";
-import { Box, IconButton, useBreakpointValue } from '@chakra-ui/react';
-// Here we have used react-icons package for the icons
-import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi';
-// And react-slick as our Carousel Lib
-import Slider from 'react-slick';
-import Data from "@/data/data";
-import { useState } from "react";
+import FirstNav from "@/components/firstNav";
+import Navbar from "@/components/navbar";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+  Label,
+  FormControl,
+  Flex,
+  FormLabel,
+  Input,
+  Link,
+  Stack,
+  Checkbox,
+  Box,
+  Image,
+  Alert,
+  useToast,
+  Toast,
+  Center,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
 
-
-// Settings for the slider
-const settings = {
-    dots: true,
-    arrows: false,
-    fade: true,
-    infinite: true,
-    autoplay: true,
-    speed: 500,
-    autoplaySpeed: 5000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
-
-
-
-export default function CatH() {
-    const [slider, setSlider] = useState(null);
-    const data=Data;
-    
-    const settings = {
-        className: "center",
-        infinite: true,
-        centerPadding: "60px",
-        slidesToShow: 6,
-        swipeToSlide: true,
-      };
-      const top = useBreakpointValue({ base: '90%', md: '50%' });
-      const side = useBreakpointValue({ base: '30%', md: '40px' });
-    const router = useRouter()
+export default function Connexion() {
   return (
     <>
-        <Flex justifyContent={'space-between'}>
-      <Flex ml={35}>
-        <Text>Home</Text>
-        <ChevronRightIcon fontSize={30}/>
-        Categories
-        <br/>   
-      </Flex>
-      <Box>
-      <IconButton
-        aria-label="left-arrow"
-        variant="ghost"
-        // position="relative"
-        left={side}
-        top={top}
-        transform={'translate(0%, -50%)'}
-        zIndex={2}
-        onClick={() => slider?.slickPrev()}>
-        <BiRightArrowAlt size="40px" />
-      </IconButton>
-      {/* Right Icon */}
-      <IconButton
-        aria-label="right-arrow"
-        variant="ghost"
-        // position="absolute"
-        right={side}
-        top={top}
-        transform={'translate(0%, -50%)'}
-        zIndex={2}
-        onClick={() => slider?.slickNext()}>
-        
-        <BiLeftArrowAlt size="40px" />
-      </IconButton>
-      </Box>
-    </Flex>
-    <Text ml={20} mb={10}>{router.pathname.replace('/','')}</Text>
-    <Box>
-    <Slider {...settings} ref={(slider) => setSlider(slider)}>
-        {Data.map((data, index) => (
-          <Box
-            maxW="sm"
-            width={"sm"}
-            height={"sm"}
-            borderWidth="1px"
-            borderRadius="lg"
-            overflow="hidden"
-            px={10}
-            boxShadow={"2xl"}
-            mx={10}
-            mb={10}
-            pb={10}
-          >
-            <Box width={'150px'} height={'170px'} pt={10} pl={10}>
-            <img src={data.images[0]} alt={data.title} />
-            </Box>
-            
-
-            <Box p="6">
-              <Box
-                mt="1"
-                fontWeight="semibold"
-                as="h5"
-                lineHeight="tight"
-                noOfLines={2}
-                w={'fit-content'}
-                height={'50px'}
-              >
-                {data.title}
-              </Box>
-
-              <Box
-                mt="1"
-                fontWeight="normal"
-                
-                lineHeight="tight"
-                noOfLines={2}
-                w={'fit-content'}
-                height={'50px'}
-              >
-                <Text>{data.description.toString()}</Text>
-              </Box>
-              <Box>
-                {data.price}
-                <Box as="span" color="gray.600" pl={2} fontSize="sm">
-                  XOF
-                </Box>
-              </Box>
-
-              <Box display="flex" mt="2" alignItems="center">
-                {Array(5)
-                  .fill("")
-                  .map((_, i) => (
-                    <StarIcon key={i} color={i < 4 ? "teal.500" : "gray.300"} />
-                  ))}
-              </Box>
-              <Box>
-                <Button bgColor={'blue'} mt={3} borderRadius={'66px'}></Button>
-              </Box>
-            </Box>
+      <FirstNav />
+      <Navbar />
+      <Center>
+        <Flex
+          bgColor={"white"}
+          borderRadius={5}
+          mt={5}
+          w={"90%"}
+          h={500}
+          mx={[5, 5, 5, 5, 12]}
+          my={20}
+          boxShadow={'0px 4px 24px '}
+          box
+        >
+          <Box h={500}>
+            <Image
+            height={500}
+            width={650}
+              src="./images/Banniere.png"
+              display={["none", "none", "flex", "flex", "flex"]}
+            />
           </Box>
-        ))}
-        {/* </SimpleGrid> */}
-      </Slider>
-    </Box>
-    
-    
-   
+          <Center>
+            <Box width={'full'} color={'black'} ml={20}>
+              <Stack spacing={4}>
+                <Heading>Bienvenue</Heading>
+                <Text>Connectez-vous á votre compte</Text>
+                <Input type={'text'} placeholder='Email' border={'2px solid gray'} borderRadius={'50px'} width={'350px'} color={'gray.500'}/>
+                <Input type={'password'} placeholder={'Mot de passe'}  border={'2px solid gray'} borderRadius={'50px'} width={'350px'}/>
+                <Button borderRadius={'50px'} bgColor={'blue.400'} color={'white'}>Connexion </Button>
+              </Stack>
+              
+            </Box>
+          </Center>
+        </Flex>
+      </Center>
     </>
-    
-    
   );
 }
